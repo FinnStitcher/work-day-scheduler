87429c262d549c8122ac77e92101b7d5f40db2f7
Finn Stitcher <foundfootage@protonmail.com>
2022-06-05 18:01:01 -0700

    initialize hourly updater

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,32 @@
+var hour = 0;
+
+function initializeTime() {
+    // set hour to current hour
+    hour = moment().hour();
+    console.log(hour);
+
+    // calculate how long until the next hour
+    var minutesToNextHour = 60 - moment().minute();
+
+    setTimeout(function () {
+        // update hour again - this is necessary to properly initialize things
+        hour = moment().hour()
+        console.log(hour);
+        // call the function that handles hourly updates
+        updateHourly();
+    }, 1000 * 60 * minutesToNextHour);
+};
+
 function displayDate() {
     var date = moment().format("dddd, MMMM Do");
     $("#currentDay").text(`Today is ${date}.`);
 };
 
+function updateHourly() {
+    setInterval(function () {
+        console.log("This is working!");
+    }, 5000);
+};
+
+initializeTime();
 displayDate();
